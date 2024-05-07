9ccd69387155d10792c354c8b6c2157ac8db96eb
Мезенцева Дарья <125379169+darmezents@users.noreply.github.com>
2024-05-07 08:13:17 +0700

    dz7

--- main.py
+++ main.py
@@ -25,26 +25,72 @@ START_ROUTES, END_ROUTES = range(2)
 # Callback data
 STAGE_ONE, STAGE_TWO, STAGE_THREE, STAGE_FOUR = range(4)
 
+def grouped(array, num=3):
+    return [array[i:i+num] for i in range(0, len(array), num)]
+
+class Section:
+    id: int
+    name: str
+    text: str
+
+    key = 'section_'
+
+    def __init__(self, id: int, name: str, text: str = ''):
+        self.id = id
+        self.name = name
+        self.text = text
+
+    def get_inline_button(self):
+        return InlineKeyboardButton(self.name, callback_data=self.key + str(self.id))
+
+class Course:
+    id: int
+    name: str
+    sections: list
+    text: str
+
+    key = 'course_'
+
+    def __init__(self, id: int, name: str, sections: list, text: str = ''):
+        self.id = id
+        self.name = name
+        self.sections = sections
+        self.text = text
+
+    def get_inline_button(self):
+        return InlineKeyboardButton(self.name, callback_data=self.key + str(self.id))
+
+COURSES = [
+    Course(1, 'Python', [Section(1, 'Урок 1'),Section(2, 'Урок 2'),Section(3, 'Урок 3')],'Курс представляет собой обучающую программу, нацеленную на изучение языка программирования Python'),
+    Course(2, 'SQL', [Section(1, 'Урок 1'),Section(2, 'Урок 2'),Section(3, 'Урок 3')], 'Курс представляет собой обучающую программу, нацеленную на изучение языка структурированных запросов'),
+    Course(3, 'PHP', [Section(1, 'Урок 1'),Section(2, 'Урок 2'),Section(3, 'Урок 3')], 'Курс представляет собой обучающую программу, нацеленную на изучение языка программирования PHP'),
+    Course(4, 'Telegram', [Section(1, 'Урок 1'),Section(2, 'Урок 2'),Section(3, 'Урок 3')], 'Курс представляет собой обучающую программу, направленную на изучение возможностей и функционала мессенджера Telegram'),
+    Course(5, 'HTML', [Section(1, 'Урок 1'),Section(2, 'Урок 2'),Section(3, 'Урок 3')], 'Курс представляет собой обучающую программу, нацеленную на изучение основ веб-разработки'),
+]
+
+def get_course_by_course_key(course_key):
+    id = int(course_key.replace(Course.key, ''))
+
+    for c in COURSES:
+        if c.id == id:
+            return c
+        
+    return None
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends a message with three inline buttons attached."""
-    keyboard = [
-        [
-            InlineKeyboardButton("Python", callback_data="1"),
-            InlineKeyboardButton("SQL", callback_data="2"),
-            InlineKeyboardButton("PHP", callback_data="3"),
-            InlineKeyboardButton("Telegram", callback_data="4"),
-            InlineKeyboardButton("HTML", callback_data="5"),
-        ],
-        [InlineKeyboardButton("Random", callback_data="0")],
-    ]
 
-
+    courses = grouped([c.get_inline_button() for c in COURSES], 3)
+    
+    keyboard = courses
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text("Выберите курс:", reply_markup=reply_markup)
+    await update.message.reply_text(
+        "Выбор курса:", reply_markup=reply_markup)
 
     return START_ROUTES
+
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Parses the CallbackQuery and updates the message text."""
@@ -58,41 +104,40 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
-async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def one(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    await update.message.reply_text("STAGE 1")
+
+async def course_detail(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Show new choice of buttons"""
     query = update.callback_query
+    course = get_course_by_course_key(query.data)
+
+    section_keyboard = grouped([s.get_inline_button() for s in course.sections], 3)
+
     await query.answer()
-    keyboard = [
-        [
-            InlineKeyboardButton("3", callback_data=str(STAGE_THREE)),
-            InlineKeyboardButton("4", callback_data=str(STAGE_FOUR)),
-        ]
-    ]
+    keyboard = section_keyboard
     reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(
-        text="First CallbackQueryHandler, Choose a route", reply_markup=reply_markup
+        text=f'Курс по: {course.name} \n{course.text}', reply_markup=reply_markup
     )
     return START_ROUTES
-
-
 
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
     TOKEN = '7112091493:AAGUL_ogltHei7o2k4AYwCEm8MO_3_wH_eA'
-
     application = Application.builder().token(TOKEN).build()
 
     # application.add_handler(CommandHandler("start", start))
+    # application.add_handler(CallbackQueryHandler(button))
+    # application.add_handler(CommandHandler("help", help_command))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
             START_ROUTES: [
-                CallbackQueryHandler(one, pattern="^" + str(STAGE_ONE) + "$"),
-                # CallbackQueryHandler(two, pattern="^" + str(TWO) + "$"),
-                # CallbackQueryHandler(three, pattern="^" + str(THREE) + "$"),
-                # CallbackQueryHandler(four, pattern="^" + str(FOUR) + "$"),
+                CallbackQueryHandler(course_detail, pattern="^" + Course.key),
             ],
             # END_ROUTES: [
                 # CallbackQueryHandler(start_over, pattern="^" + str(ONE) + "$"),
@@ -107,7 +152,6 @@ def main() -> None:
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
